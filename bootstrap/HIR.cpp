46d62dd22bbb9e8ff8cee1e6aa7ea0a750f77da2
Ronie Salgado <ronie@desromech.cl>
2026-05-01 20:46:39 -0400

    HIRValue.

---
+++ bootstrap/HIR.cpp
@@ -0,0 +1,1 @@
+#include "HIR.hpp"
---
+++ bootstrap/HIR.hpp
@@ -0,0 +1,8 @@
+#ifndef SYSMEL_HIR_HPP
+#define SYSMEL_HIR_HPP
+
+#include "Value.hpp"
+
+typedef std::shared_ptr<struct HIRValue> HIRValuePtr;
+
+#endif //SYSMEL_HIR_HPP
