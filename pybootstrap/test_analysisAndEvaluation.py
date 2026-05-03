3ebbc6aafe9a2d293dd657c4bb9329d04c98b76d
Ronie Salgado <ronie@desromech.cl>
2026-05-03 15:19:35 -0400

    Missing files from previous commit.

---
+++ pybootstrap/test_analysisAndEvaluation.py
@@ -0,0 +1,72 @@
+import unittest
+from parsetree import *
+from parser import parseSourceString
+from analysisAndEvaluation import *
+from hir import *
+
+class TestAnalysisAndEvaluation(unittest.TestCase):
+    def setUp(self):
+        self.context = HIRContext()
+        self.package = HIRPackage('Test')
+        self.package.usePackage(self.context.corePackage)
+        self.context.currentPackage = self.package
+        return super().setUp()
+    
+    def parseSourceStringWithoutErrors(self, string: str) -> ParseTreeNode:
+        ast = parseSourceString(string)
+        self.assertTrue(ParseTreeErrorVisitor().checkAndPrintErrors(ast))
+        return ast
+    
+    def evaluateTopLevelSourceString(self, string: str):
+        ast = self.parseSourceStringWithoutErrors(string)
+        evaluationContext = self.context.createTopLevelEvaluationContext(ast.sourcePosition.sourceCode)
+        return AnalysisAndEvaluationPass(evaluationContext).visitDecayedNode(ast)
+
+    def testEmptyTopLevel(self):
+        topLevelResult = self.evaluateTopLevelSourceString('')
+        self.assertTrue(topLevelResult.isVoidConstant())
+
+    def testLiteralInteger(self):
+        topLevelResult = self.evaluateTopLevelSourceString('42')
+        self.assertTrue(topLevelResult.isIntegerConstant())
+        self.assertEqual(topLevelResult.value, 42)
+
+    def testLiteralFloat(self):
+        topLevelResult = self.evaluateTopLevelSourceString('42.5')
+        self.assertTrue(topLevelResult.isFloatConstant())
+        self.assertEqual(topLevelResult.value, 42.5)
+
+    def testLiteralCharacter(self):
+        topLevelResult = self.evaluateTopLevelSourceString("'A'")
+        self.assertTrue(topLevelResult.isCharacterConstant())
+        self.assertEqual(topLevelResult.value, ord('A'))
+
+    def testLiteralString(self):
+        topLevelResult = self.evaluateTopLevelSourceString('"Hello World"')
+        self.assertTrue(topLevelResult.isStringConstant())
+        self.assertEqual(topLevelResult.value, 'Hello World')
+
+    def testLiteralSymbol(self):
+        topLevelResult = self.evaluateTopLevelSourceString('#hello')
+        self.assertTrue(topLevelResult.isSymbolConstant())
+        self.assertEqual(topLevelResult.value, 'hello')
+
+    def testLexicalBlock(self):
+        topLevelResult = self.evaluateTopLevelSourceString('{42}')
+        self.assertTrue(topLevelResult.isIntegerConstant())
+        self.assertEqual(topLevelResult.value, 42)
+
+    def testPackageSymbols(self):
+        topLevelResult = self.evaluateTopLevelSourceString('false')
+        self.assertTrue(topLevelResult.isBooleanConstant())
+        self.assertFalse(topLevelResult.value)
+        
+        topLevelResult = self.evaluateTopLevelSourceString('true')
+        self.assertTrue(topLevelResult.isBooleanConstant())
+        self.assertTrue(topLevelResult.value)
+
+        topLevelResult = self.evaluateTopLevelSourceString('void')
+        self.assertTrue(topLevelResult.isVoidConstant())
+
+        topLevelResult = self.evaluateTopLevelSourceString('nil')
+        self.assertTrue(topLevelResult.isNilConstant())
