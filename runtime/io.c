e9a8eaaa89abea49fce01bda4a8748d720921137
Ronie Salgado <ronie@desromech.cl>
2026-05-07 17:32:44 -0400

    IO stub.

--- runtime/io.c
+++ runtime/io.c
@@ -1,7 +1,17 @@
 #include "common.h"
 #include <stdio.h>
 
+void __sysmel_io_print(sysmel_oop_t oop)
+{
+    printf("TODO: __sysmel_io_print %p\n", (void*)oop);
+}
+
 void __sysmel_io_printLine(sysmel_oop_t oop)
 {
     printf("TODO: __sysmel_io_printLine %p\n", (void*)oop);
+}
+
+void __sysmel_io_writeLine(sysmel_oop_t oop)
+{
+    printf("TODO: __sysmel_io_writeLine %p\n", (void*)oop);
 }
